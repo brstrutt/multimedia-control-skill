fd347173b70815104ae2551d4af95b4ad0ba286c
isienmai <ben.strutt@hotmail.co.uk>
2021-03-10 17:04:27 +0000

    Add some more detailed info to the readme, and implemented the basic play/pause/skip controls

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ Play/pause/skip currently playing media
 ## About
 Control music/videos currently playing on your computer, much like using media control keys on a keyboard. uses the playback control skill as the trigger for play/pause/next/previous commands, which this skill simply passes on to whatever media is active on the local device at the time.
 
+Extra Info: This skill simply translates play/pause/next/etc controls from the playback controls skill into MPRIS2 controls which are supported by a wide array of media players. It only sends commands to one MPRIS2 supported application at a time. Currently it is a bit pot-luck as to which application it controls if there are several running.
+
 ## Examples
 * "Next song."
 * "Pause."
--- __init__.py
+++ __init__.py
@@ -1,13 +1,42 @@
 from mycroft import MycroftSkill, intent_file_handler
-
+import mpris2
 
 class MultimediaControl(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
+        this.mediaPlayer = None
 
-    @intent_file_handler('control.multimedia.intent')
-    def handle_control_multimedia(self, message):
-        self.speak_dialog('control.multimedia')
+        # Grab the first URI in the list. This will just grab whatever media player happens to be first.
+        try:
+            uri = next(mpris2.get_players_uri())
+            this.mediaPlayer = mpris2.Player(dbus_interface_info={'dbus_uri': uri})
+        except StopIteration:
+            pass
+    
+    def handler_mycroft_audio_service_play(self, message):
+        if this.mediaPlayer != None:
+            this.mediaPlayer.Play()
+
+    def handler_mycroft_audio_service_stop(self, message):
+        if this.mediaPlayer != None:
+            this.mediaPlayer.Pause()
+
+    def handler_mycroft_audio_service_pause(self, message):
+        if this.mediaPlayer != None:
+            this.mediaPlayer.Pause()
+
+    def handler_mycroft_audio_service_resume(self, message):
+        if this.mediaPlayer != None:
+            this.mediaPlayer.Play()
+
+    def handler_mycroft_audio_service_next(self, message):
+        if this.mediaPlayer != None:
+            this.mediaPlayer.Next()
+
+    def handler_mycroft_audio_service_prev(self, message):
+        if this.mediaPlayer != None:
+            this.mediaPlayer.Previous()
+
 
 
 def create_skill():
