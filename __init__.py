bb4afafb8b39e533ee2c8ae3261dcfe7de01c0c9
isienmai <ben.strutt@hotmail.co.uk>
2021-03-10 18:13:34 +0000

    Fix errors in the python syntax

--- __init__.py
+++ __init__.py
@@ -4,46 +4,60 @@ import mpris2
 class MultimediaControl(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
-        this.mediaPlayer = None
+        self.mediaPlayer = None
 
     def initialize(self):
         # Grab the first URI in the list. This will just grab whatever media player happens to be first.
         try:
             uri = next(mpris2.get_players_uri())
-            this.mediaPlayer = mpris2.Player(dbus_interface_info={'dbus_uri': uri})
+            self.mediaPlayer = mpris2.Player(dbus_interface_info={'dbus_uri': uri})
         except StopIteration:
             pass
         self.log.info("Multimedia Control started. Controlling URI: " + str(uri))
+        
+        # Bind to the various audio service messages
+        self.add_event('mycroft.audio.service.play',
+                   self.handler_mycroft_audio_service_play)
+        self.add_event('mycroft.audio.service.stop',
+                   self.handler_mycroft_audio_service_stop)
+        self.add_event('mycroft.audio.service.pause',
+                   self.handler_mycroft_audio_service_pause)
+        self.add_event('mycroft.audio.service.resume',
+                   self.handler_mycroft_audio_service_resume)
+        self.add_event('mycroft.audio.service.next',
+                   self.handler_mycroft_audio_service_next)
+        self.add_event('mycroft.audio.service.prev',
+                   self.handler_mycroft_audio_service_prev)
     
     def handler_mycroft_audio_service_play(self, message):
-        self.log.inf("mycroft audio service play received")
-        if this.mediaPlayer != None:
-            this.mediaPlayer.Play()
+        self.log.info("mycroft audio service play received")
+        if self.mediaPlayer != None:
+            self.mediaPlayer.Play()
 
     def handler_mycroft_audio_service_stop(self, message):
-        self.log.inf("mycroft audio service stop received")
-        if this.mediaPlayer != None:
-            this.mediaPlayer.Pause()
+        self.log.info("mycroft audio service stop received")
+        if self.mediaPlayer != None:
+            self.mediaPlayer.Pause()
 
     def handler_mycroft_audio_service_pause(self, message):
-        self.log.inf("mycroft audio service pause received")
-        if this.mediaPlayer != None:
-            this.mediaPlayer.Pause()
+        self.log.info("mycroft audio service pause received")
+        if self.mediaPlayer != None:
+            self.mediaPlayer.Pause()
 
     def handler_mycroft_audio_service_resume(self, message):
-        self.log.inf("mycroft audio service resume received")
-        if this.mediaPlayer != None:
-            this.mediaPlayer.Play()
+        self.log.info("mycroft audio service resume received")
+        if self.mediaPlayer != None:
+            self.mediaPlayer.Play()
 
     def handler_mycroft_audio_service_next(self, message):
-        self.log.inf("mycroft audio service next received")
-        if this.mediaPlayer != None:
-            this.mediaPlayer.Next()
+        self.log.info("mycroft audio service next received")
+        if self.mediaPlayer != None:
+            self.mediaPlayer.Next()
 
     def handler_mycroft_audio_service_prev(self, message):
         self.log.inf("mycroft audio service prev received")
-        if this.mediaPlayer != None:
-            this.mediaPlayer.Previous()
+        if self.mediaPlayer != None:
+            self.mediaPlayer.Previous()
 
 
 
